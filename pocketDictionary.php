39aea4a5877cfed65080b6a6e9894f75689169db
Anthonymutila <mutilaantony@yahoo.com>
2026-02-23 20:22:08 +0200

    added files statc where styles reside and  some includes

--- pocketDictionary.php
+++ pocketDictionary.php
@@ -8,69 +8,65 @@
 <body>
 
 <div class="container">
-    
-    <!-- Search form here -->
-    <form action="<?php  htmlspecialchars( $_SERVER['PHP_SELF']);?>" method="post">
-            <div class="search-box">
-                <input type="text" name="wordToSearch" placeholder="Search for any word..." />
+    <!--header-->
+    <?php include("templates/header.html");?>
+     
+
+
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"]=="POST"){
+        if (empty($_POST['wordToSearch'])) {
+            echo '<div class="error"><p>Please enter the word to search</p></div>';
+        }else{
+            $word = $_POST['wordToSearch'];  // word to search 
+            $url = "https://api.dictionaryapi.dev/api/v2/entries/en/".urlencode($word);
             
-            </div>
-    </form>
- 
-
-<?php
-
-if ($_SERVER["REQUEST_METHOD"]=="POST"){
-    if (empty($_POST['wordToSearch'])) {
-        echo '<p>Please enter the word to search</p>';
-    }else{
-        $word = $_POST['wordToSearch'];  // word to search 
-        $url = "https://api.dictionaryapi.dev/api/v2/entries/en/".urlencode($word);
-        // data coming from the form
-        $response = file_get_contents($url);
-        $data = json_decode($response,True);
-        ?> 
-        <!-- Word Header -->
-        <div class='word-header'>
-            <div>
-                <h1><?php echo $word; ?></h1>
-                <p class='phonetic'>/ɪɡˈzɑːmpəl/</p>
-            </div>
-            <div class='play-btn'>▶</div>
-        </div>
-        <?php
-        foreach ($data[0]['meanings'] as $meaning) {
-            ?>
-            <div class='section'><!-- START section -->
-                <div class='section-title'>
-                    <span><?php echo $meaning['partOfSpeech']; ?></span>
-                    <div class='line'></div>
+            // data coming from the form
+            $response = file_get_contents($url);
+            $data = json_decode($response,True);
+            ?> 
+            <!-- Word Header -->
+            <div class='word-header'>
+                <div>
+                    <h1><?php echo $word; ?></h1>
+                    <p class='phonetic'>/ɪɡˈzɑːmpəl/</p>
                 </div>
-        
-            <ul>
-             <p class="meaning-label">Meaning</p>
-            <?php   
-            foreach ($meaning['definitions'] as $definition) {
+                <div class='play-btn'>▶</div>
+            </div>
+            <?php
+            foreach ($data[0]['meanings'] as $meaning) {
                 ?>
-               
+                <div class='section'><!-- START section -->
+                    <div class='section-title'>
+                        <span><?php echo $meaning['partOfSpeech']; ?></span>
+                        <div class='line'></div>
+                    </div>
+            
+                <ul>
+                <p class="meaning-label">Meaning</p>
+                <?php   
+                foreach ($meaning['definitions'] as $definition) {
+                    ?>
                 
-                    <li><?php echo $definition['definition'];?></li>
-                <?php    
-                     if (isset($definition['example'])) {
+                    
+                        <li><?php echo $definition['definition'];?></li>
+                    <?php    
+                        if (isset($definition['example'])) {
+                    ?>
+                    <p><em><b>Example: <?php echo $definition['example'];?></b></em></p>
+                <?php
+                }  }
                 ?>
-                <p><em><b>Example: <?php echo $definition['example'];?></b></em></p>
-            <?php
-            }  }
-            ?>
-            </ul></div><!-- END section -->
-            <?php
-             
-        }
-}
-    
-}
+                </ul></div><!-- END section -->
+                <?php
+                
+            }
+    }
+        
+    }
 
-?>
+    ?>
 
 </div><!--END CONTAINER-->
 
